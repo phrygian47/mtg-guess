ae23a0632bb67a6ed6a69b1c11d2f5db1840b0ef
Robert Elliott <robert.elliott2925@gmail.com>
2026-04-17 15:37:10 -0700

    Testing Cron - Testing DB

--- src/app/api/cron/scryfall-sync/route.ts
+++ src/app/api/cron/scryfall-sync/route.ts
@@ -1,30 +1,5 @@
 import { importScryfallCards } from "@/lib/scryfall/scryfall-import";
 
-// export async function GET(req: Request) {
-//   const auth = req.headers.get("authorization");
-
-//   if (auth !== `Bearer ${process.env.CRON_SECRET}`) {
-//     return new Response("Unauthorized", { status: 401 });
-//   }
-
-//   try {
-//     const result = await importScryfallCards();
-//     return Response.json({ ok: true, result });
-//   } catch (error) {
-//     console.error("Cron sync failed:", error);
-
-//     return Response.json(
-//       {
-//         ok: false,
-//         error: error instanceof Error ? error.message : "Unknown error",
-//       },
-//       { status: 500 },
-//     );
-//   }
-
-// }
-import { sql } from "@/lib/db/db";
-
 export async function GET(req: Request) {
   const auth = req.headers.get("authorization");
 
@@ -33,9 +8,11 @@ export async function GET(req: Request) {
   }
 
   try {
-    const result = await sql`select now() as now`;
-    return Response.json({ ok: true, db: result[0] });
+    const result = await importScryfallCards();
+    return Response.json({ ok: true, result });
   } catch (error) {
+    console.error("Cron sync failed:", error);
+
     return Response.json(
       {
         ok: false,
@@ -45,3 +22,25 @@ export async function GET(req: Request) {
     );
   }
 }
+// import { sql } from "@/lib/db/db";
+
+// export async function GET(req: Request) {
+//   const auth = req.headers.get("authorization");
+
+//   if (auth !== `Bearer ${process.env.CRON_SECRET}`) {
+//     return new Response("Unauthorized", { status: 401 });
+//   }
+
+//   try {
+//     const result = await sql`select now() as now`;
+//     return Response.json({ ok: true, db: result[0] });
+//   } catch (error) {
+//     return Response.json(
+//       {
+//         ok: false,
+//         error: error instanceof Error ? error.message : "Unknown error",
+//       },
+//       { status: 500 },
+//     );
+//   }
+// }
