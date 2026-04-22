12a70764c5219c061ebef6486fcc89a66a117a8b
Robert Elliott <robert.elliott2925@gmail.com>
2026-04-22 10:33:38 -0700

    Changed random card query for fetch

--- src/lib/scryfall/save-selected.ts
+++ src/lib/scryfall/save-selected.ts
@@ -2,7 +2,7 @@ import type { Card } from "@/lib/scryfall/types";
 
 export async function fetchRandomSelectedCard(): Promise<Card> {
   const res = await fetch(
-    "https://api.scryfall.com/cards/random?q=game%3Apaper+lang%3Aen+legal%3Astandard+-is%3Apromo+-border%3Agold+-border%3Asilver",
+    "https://api.scryfall.com/cards/random?q=game%3Apaper+lang%3Aen+legal%3Astandard+-is%3Apromo+-border%3Agold+-border%3Asilver+-is%3Amulti",
     {
       cache: "no-store",
     },
